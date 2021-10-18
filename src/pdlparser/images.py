8e2d4f4844093bb8468a64d3b78928f32a6dba62
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-10-18 11:24:35 -0500

    Generate vertical and horizontal lines
    Function to bring out vertical and horizontal line elements of the
    tables within an image.

--- src/pdlparser/images.py
+++ src/pdlparser/images.py
@@ -40,3 +40,36 @@ def generate_kernels(img_array: npt.NDArray):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 
     return kernel, vert_kernel, hori_kernel
+
+
+def generate_vh_lines(img: Image) -> npt.NDArray:
+    """Given a PIL image object create a new image with just table outline
+    elements.
+
+    This function takes an image and uses errosion and dilation to bring out
+    vertical and horizontal lines of tables.
+
+    Args:
+        img: PIL image object
+
+    Returns:
+        Numpy array with just table elements.
+
+    """
+    img_bin = generate_img_bin(img)
+    kernel, vert_kernel, hori_kernel = generate_kernels(img_bin)
+
+    v1 = cv2.erode(img_bin, vert_kernel, iterations=3,)
+    vert_lines = cv2.dilate(v1, vert_kernel, iterations=3,)
+
+    h1 = cv2.erode(img_bin, hori_kernel, iterations=3,)
+    hori_lines = cv2.dilate(h1, hori_kernel, iterations=3,)
+
+    img_vh = cv2.addWeighted(vert_lines, 0.5, hori_lines, 0.5, 0)
+    img_vh = cv2.erode(~img_vh, kernel, iterations=2)
+
+    _, img_vh = cv2.threshold(img_vh, 128, 255, cv2.THRESH_BINARY)
+
+    img_vh = cv2.cvtColor(img_vh, cv2.COLOR_BGR2GRAY)
+
+    return img_vh
